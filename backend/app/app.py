ea8c57990309863fbbcad00450a824f732740f7e
Kristal Garcia <kristal.garcia@buzzfeed.com>
2020-03-01 12:22:20 -0800

    Add health check and inits

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
+
+
+@app.route('/health')
+def health_check():
+    return "ok", 200
 
 
 if __name__ == '__main__':
